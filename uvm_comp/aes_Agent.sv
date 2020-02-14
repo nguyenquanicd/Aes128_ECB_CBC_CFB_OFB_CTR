670b426276fb24d03105eeb276b3f93a5c51ecac
thanhvanphan <58868395+thanhvanphan@users.noreply.github.com>
2020-02-15 05:13:22 +0700

    Modify and update

--- uvm_comp/aes_Agent.sv
+++ uvm_comp/aes_Agent.sv
@@ -1,23 +1,37 @@
+//--------------------------------------
+//Project  :  The UVM environemnt for AES128
+//File name:  aes_Sequencer.sv
+//Author   :  Phan Van Thanh
+//Page     :  VLSI Technology
+//--------------------------------------
 class aes_Agent extends uvm_agent;
-  //Register to Factory
-  `uvm_component_utils(aes_Agent)
-  //Declare Sequencer and Driver
-  aes_Driver    aes_Driver_inst;
-  aes_Sequencer aes_Sequencer_inst;
-  //Constructor
-  function new(string name = "aes_Agent", uvm_component parent);
-      super.new(name, parent);
-  endfunction
-  //Build Driver and Sequencer objects
-  function void build_phase(uvm_phase phase);
-      super.build_phase(phase);
-      aes_Driver_inst    = aes_Driver::type_id::create("aes_Driver_inst",this);
-      aes_Sequencer_inst = aes_Sequencer::type_id::create("aes_Sequencer_inst",this);
-  endfunction
-  //Connect Driver and Sequencer via a couple export/port
-  function void connect_phase(uvm_phase phase);
-      super.connect_phase(phase);
-      aes_Driver_inst.seq_item_port.connect(aes_Sequencer_inst.seq_item_export);
-  endfunction
-  //
+    //Register to Factory
+    `uvm_component_utils(aes_Agent)
+    
+    //Declare Sequencer and Driver
+    aes_Driver      aes_Driver_inst;
+    aes_Sequencer   aes_Sequencer_inst;
+    aes_Monitor     aes_Monitor_inst;
+    
+    //Constructor
+    function new(string name = "aes_Agent", uvm_component parent);
+        super.new(name, parent);
+    endfunction
+  
+    //Build phase
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        aes_Driver_inst    = aes_Driver::type_id::create("aes_Driver_inst",this);
+        aes_Sequencer_inst = aes_Sequencer::type_id::create("aes_Sequencer_inst",this);
+		aes_Monitor_inst   = aes_Monitor::type_id::create("aes_Sequencer_inst",this);
+    endfunction
+  
+    //Connect phase
+	//Connect the driver seq_item_port to sequencer seq_item_export 
+	//for communication between driver and sequencer in the connect phase
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        aes_Driver_inst.seq_item_port.connect(aes_Sequencer_inst.seq_item_export);
+    endfunction
+  
 endclass: aes_Agent
---
+++ uvm_comp/aes_Test.sv
@@ -0,0 +1,30 @@
+//--------------------------------------
+//Project  :  The UVM environemnt for AES128
+//File name:  aes_Test.sv
+//Author   :  Nguyễn Hùng Quân, Phan Văn Thành, Nguyễn Thành Công, Trần Hữu Toàn
+//Page     :  VLSI Technology
+//--------------------------------------
+
+class aes_Test extends uvm_test;
+    `uvm_component_utils(aes_Test);
+	aes_Env        aes_Env_inst;
+	aes_Sequence   aes_Sequence_inst;
+	
+	function new(string name = "aes_Test",uvm_component parent=null);
+	    super.new(name,parent);
+	endfunction
+	
+	virtual function void build_phase(uvm_phase phase);
+	    super.build_phase(phase);
+		
+		aes_Env_inst      = aes_Env::type_id::create("aes_env",this);
+		aes_Sequence_inst = aes_Sequence::type_id::create("aes_Sequence_inst",this)
+	endfunction
+	
+	task run_phase(uvm_phase phase);
+	    phase.raise_objection(this);
+		seq.start(aes_Env_inst.aes_Agent_inst.aes_Sequence_inst);
+		phase.drop_objection(this)
+	endtask
+	
+endclass
