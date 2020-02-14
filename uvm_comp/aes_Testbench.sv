f610d7a1f99d0d3dd59d0f8a9cedd794a7e7b9ca
thanhvanphan <58868395+thanhvanphan@users.noreply.github.com>
2020-02-15 06:12:00 +0700

    Update include file

--- uvm_comp/aes_Testbench.sv
+++ uvm_comp/aes_Testbench.sv
@@ -4,9 +4,45 @@
 //Author   :  Nguyễn Hùng Quân, Phan Văn Thành, Nguyễn Thành Công, Trần Hữu Toàn
 //Page     :  VLSI Technology
 //--------------------------------------
+`include "uvm_pkg.sv"
+`include "uvm_macros.svh"
+`include "uMacro.svh"
+
 
 module aes_Testbench;
 
+    //Import UVM package
+    import uvm_pkg::*;
+	`include "aes.c"
+    `include "aes.h"
+    `include "aes_Agent.sv"
+    `include "aes_Driver.sv"
+    `include "aes_Env.sv"
+    `include "aes_Interface.sv"
+    `include "aes_Monitor.sv"
+    `include "aes_Scoreboard.sv"
+    `include "aes_Sequence.sv"
+    `include "aes_Sequencer.sv"
+    `include "aes_Test.sv"
+    `include "aes_Testbench.sv"
+    `include "aes_Transaction.sv"
+    `include "aes128.sv"
+    `include "aes128_cipher_core.sv"
+    `include "aes128_cipher_core_inv.sv"
+    `include "aes128_cipher_inv_top.sv"
+    `include "aes128_cipher_top.sv"
+    `include "aes128_key_expansion.sv"
+    `include "aes128_key_expansion_inv.sv"
+    `include "aes128_mul.sv"
+    `include "aes128_mul_inv.sv"
+    `include "aes128_rcon.sv"
+    `include "aes128_rcon_inv.sv"
+    `include "aes128_sbox.sv"
+    `include "aes128_sbox_inv.sv"
+    `include "main_aes.c"
+
+	 
+	 
     //Clock and reset signal declaration
 	bit clk;
 	bit reset;
